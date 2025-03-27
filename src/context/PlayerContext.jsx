2bfb2638c0a1e69c6eec0b0a0417fd676ebff852
Jadon <1tenletters0@gmail.com>
2025-03-27 11:06:39 -0700

    I just updating what I've done so far. I got familiar with Tailwind, styled the navbar, sidebar, display, the album dislay, pretty much everthing...
    I'm working on the player functionality so it will look and work as it should.
    
    ***Plan to Update more songs so there's more versatility***

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -40,7 +40,7 @@ const PlayerContextProvider = (props) => {
   const previous = async () => {
     if (track.id>0) {
       await setTrack(songsData[track.id-1])
-      await audioRef.current.play()
+      await audioRef.current.play("")
     }
   }
 
