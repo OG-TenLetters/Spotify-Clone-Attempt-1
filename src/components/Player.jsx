91d2bf8ad2b505349b5063bd82b38433046e0b1a
Jadon <1tenletters0@gmail.com>
2025-05-30 12:53:19 -0700

    Touched up the styling and transitions.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -17,14 +17,14 @@ const Player = () => {
       </div>
       <div className="flex flex-col items-center gap-1 m-auto">
         <div className="flex gap-4">
-          <img className="w-4" src={assets.shuffle_icon} alt="" />
-          <img onClick={previous} className="w-4" src={assets.prev_icon} alt="" />
+          <img className="w-4 hover:scale-115 transition-scale duration-300 my-2 cursor-not-allowed" src={assets.shuffle_icon} alt="" />
+          <img onClick={previous} className="w-4 hover:scale-115 transition-scale duration-300 my-2 cursor-pointer" src={assets.prev_icon} alt="" />
           {playStatus 
-            ?<img onClick={pause} className="w-4" src={assets.pause_icon} alt="" />
-            :<img onClick={play} className="w-4" src={assets.play_icon} alt="" />
+            ?<div className="p-2 cursor-pointer flex justify-center items-center hover:bg-green-800 hover:scale-125 transition-all duration-300 rounded-full"><img onClick={pause} className="w-4 transition-scale duration-300" src={assets.pause_icon} alt="" /></div>
+            :<div className="p-2 cursor-pointer flex justify-center items-center hover:bg-green-800 hover:scale-125 transition-all duration-300 rounded-full"><img onClick={play} className="w-4 transition-scale duration-300" src={assets.play_icon} alt="" /></div>
           }
-          <img onClick={next} className="w-4" src={assets.next_icon} alt="" />
-          <img className="w-4" src={assets.loop_icon} alt="" />
+          <img onClick={next} className="w-4 hover:scale-115 transition-scale duration-300 my-2 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 hover:scale-115 transition-scale duration-300 my-2 cursor-not-allowed" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
           <p>{time.currentTime.minute}:{time.currentTime.second}</p>
@@ -40,15 +40,15 @@ const Player = () => {
           <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
-      <div className="hidden lg:flex items-center gap-2 opacity-75">
-        <img className="w-4" src={assets.plays_icon} alt="" />
-        <img className="w-4" src={assets.mic_icon} alt="" />
-        <img className="w-4" src={assets.queue_icon} alt="" />
-        <img className="w-4" src={assets.speaker_icon} alt="" />
-        <img className="w-4" src={assets.volume_icon} alt="" />
+      <div className="hidden lg:flex items-center gap-2 opacity-75 cursor-not-allowed">
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.plays_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.mic_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.queue_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.speaker_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.volume_icon} alt="" />
         <div className="w-20 bg-slate-50 h-1 rounded"></div>
-        <img className="w-4" src={assets.mini_player_icon} alt="" />
-        <img className="w-4" src={assets.zoom_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.mini_player_icon} alt="" />
+        <img className="w-4 hover:scale-115 transition-scale duration-300" src={assets.zoom_icon} alt="" />
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,15 +3,20 @@ import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
-      <div onClick={() => {navigate("/")}} className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+      <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around overflow-hidden">
+        <div
+          onClick={() => {
+            navigate("/");
+          }}
+          className="hover:bg-neutral-800 transition-colors duration-150 p-6 flex items-center gap-3 pl-8 cursor-pointer"
+        >
           <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold">Home</p>
         </div>
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div className="hover:bg-neutral-800 transition-colors duration-150 p-6 flex items-center gap-3 pl-8 cursor-not-allowed">
           <img className="w-6" src={assets.search_icon} alt="" />
           <p className="font-bold">Search</p>
         </div>
@@ -22,22 +27,30 @@ const Sidebar = () => {
             <img className="w-8" src={assets.stack_icon} alt="" />
             <p className="font-semibold">Your Library</p>
           </div>
-          <div className="flex items-center gap-3">
-            <img className="w-5" src={assets.arrow_icon} alt="" />
-            <img className="w-5" src={assets.plus_icon} alt="" />
+          <div className="flex items-center gap-3 cursor-not-allowed">
+            <img
+              className="w-5 hover:scale-110 transition-scale duration-300"
+              src={assets.arrow_icon}
+              alt=""
+            />
+            <img
+              className="w-5 hover:scale-110 transition-scale duration-300"
+              src={assets.plus_icon}
+              alt=""
+            />
           </div>
         </div>
-        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 ">
           <h1>Create your first playlist</h1>
           <p className="font-light">It's easy we will help you</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 hover:-translate-y-0.5 transition-translate duration-300 cursor-not-allowed">
             Create Playlist
           </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
           <h1>Let's findsome podcasts to follow</h1>
           <p className="font-light">We'll keep you updated on new episodes</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 hover:-translate-y-0.5 transition-translate duration-300 cursor-not-allowed">
             Browse Podcasts
           </button>
         </div>
